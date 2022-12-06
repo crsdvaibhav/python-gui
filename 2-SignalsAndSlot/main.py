716e89304242339f548543a8a2fd059126db4b73
crsdvaibhav <vaibhav.krpandey.che21@itbhu.ac.in>
2022-12-06 16:32:40 +0530

    SlidersAndSlots-2

--- 2-SignalsAndSlot/main.py
+++ 2-SignalsAndSlot/main.py
@@ -3,11 +3,13 @@
 from PySide6.QtWidgets import QApplication, QPushButton
 
 #The slot (like a controller function)
-def button_clicked():
-    print("You clicked the button!")
+
+def button_clicked(data): #Because the clicked signal sends the parameter
+    print("You clicked the button! checked : ", data)
 
 app = QApplication()
 button = QPushButton("Push me!")
+button.setCheckable(True) #Checkable button now
 
 button.clicked.connect(button_clicked)
 # Clicked is like a button action, onClick and connect tells what to do after click
