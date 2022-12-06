c4d14f41b8f539cd1e4b554a1d73d70a69e4a5b6
crsdvaibhav <vaibhav.krpandey.che21@itbhu.ac.in>
2022-12-06 16:03:52 +0530

    V2: In classes but still in single file

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-#V1 : Everything in global scope
+#V2 : In classes but still in single file
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 import sys #command line options
 
+class ButtonHolder(QMainWindow):
+
+    #Constructor function, self is a window object as it inherits from QMainWindow
+    
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Button holder app!")
+        button = QPushButton("Push me!")
+
+        #Setting the button as central widget
+
+        self.setCentralWidget(button)
+
 app = QApplication(sys.argv)
 
-window = QMainWindow()
-window.setWindowTitle("Our first MainWindow app!")
-
-button = QPushButton()
-button.selfText("Push me!")
-
-window.setCentralWidget(button)
-
+window = ButtonHolder()
 window.show()
-app.exec() #Start the event loop (responsible for reactivity)
 
+app.exec()
 
