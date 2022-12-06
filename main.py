8540b627d04fa91e283cf68f75b3b8f8fbf2c197
crsdvaibhav <vaibhav.krpandey.che21@itbhu.ac.in>
2022-12-06 15:48:17 +0530

    Starting with all in global scope

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#V1 : Everything in global scope
 
-print("C++ is better")
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+import sys #command line options
+
+app = QApplication(sys.argv)
+
+window = QMainWindow()
+window.setWindowTitle("Our first MainWindow app!")
+
+button = QPushButton()
+button.selfText("Push me!")
+
+window.setCentralWidget(button)
+
+window.show()
+app.exec() #Start the event loop (responsible for reactivity)
+
+
